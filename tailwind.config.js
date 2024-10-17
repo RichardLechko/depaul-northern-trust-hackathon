86d078c9a28fbdd4fffc466d8e934549e25d60d2
Richard Lechko <richardlechko04@gmail.com>
2024-10-17 11:49:16 -0500

    initialize astro + tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
