5ceacb6afdb7e1ef3241dabaa1145eb7309dc8a0
Richard Lechko <richardlechko04@gmail.com>
2024-10-17 14:11:15 -0500

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'; // Import the React integration
 
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-      }
+  integrations: [react()], // Use react() to enable React support
+  devToolbar: {
+    enabled: false,
+  },
 });
